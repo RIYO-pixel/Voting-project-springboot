c21825da71dc862eea98681ab8138f9bae753685
SHAHID SHAHRIYOR <er.shahriyor01@gmail.com>
2025-12-14 23:39:03 +0530

    Update Myconfig.java

--- src/main/java/com/voting/votingproject/config/Myconfig.java
+++ src/main/java/com/voting/votingproject/config/Myconfig.java
@@ -7,40 +7,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class Myconfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-
-            String frontendUrl = System.getProperty("FRONTEND_URL");
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")  // Applies to all endpoints
-                        .allowedOrigins(frontendUrl)  // Allow frontend origin
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed methods
-                        .allowedHeaders("*")  // Allows all headers
-                        .allowCredentials(true); // Allow credentials for authentication
-            }
-        };
-    }
-
-    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+
+        String frontendUrl = System.getenv("FRONTEND_URL");
+
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(System.getProperty("FRONTEND_URL"))); // Allow frontend
+        configuration.setAllowedOrigins(List.of(frontendUrl));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true); // Required for OAuth
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
 }
